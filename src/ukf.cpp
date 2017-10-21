75091b6b3c002a78b6da98d17d860fca66519e06
bauer_san <geoffbauer@gmail.com>
2017-10-21 08:31:29 -0400

    prediction and laser update are working. radar update needs to be verified. Need to add calculations for NIS.

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -3,7 +3,7 @@
 #include "Eigen/Dense"
 #include <iostream>
 
-#define TEST 0
+#define TEST 1
 
 using namespace std;
 using Eigen::MatrixXd;
@@ -38,23 +38,25 @@ UKF::UKF() {
 
   // initial state vector
   x_ = VectorXd(n_x_);
-//  x_.fill(0.);
+  x_.fill(0.);
  
   //matrix to hold the sigma points
   Xsig_aug = MatrixXd(n_aug_, n_sig_);
+  Xsig_aug.fill(0.);
 
   //matrix to hold the "k+1" predicted sigma points
   Xsig_pred_ = MatrixXd(n_x_, n_sig_);
-  
+  Xsig_pred_.fill(0.);
+
   // initial covariance matrix
   P_ = MatrixXd(n_x_, n_x_);
   P_.fill(0.);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 2; //30;
+  std_a_ = 0.2; // 2; //30;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 10*M_PI/180; //30;
+  std_yawdd_ = 0.2; // 10 * M_PI / 180; //30;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
@@ -91,7 +93,7 @@ UKF::~UKF() {}
  * either radar or laser.
  */
 void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
-  
+
   if (!is_initialized_) {// initialize to the first measurement
     switch  (meas_package.sensor_type_) {
 	  case MeasurementPackage::LASER:
@@ -120,8 +122,8 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
 	  default:
 	    std::cout << "UNKNOWN SENSOR TYPE: " << meas_package.sensor_type_ << std::endl;
     }
-  //std::cout << "x_ after initialization:\n" << x_ << std::endl;
-  //std::cout << "P_ after initialization:\n" << P_ << std::endl;
+//  std::cout << "x_ after initialization:\n" << x_ << std::endl;
+//  std::cout << "P_ after initialization:\n" << P_ << std::endl;
   } else {
     
     Prediction((meas_package.timestamp_ - time_us_)/1E6);
@@ -140,9 +142,11 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   // Update time
   time_us_ = meas_package.timestamp_;
 
-std::cout << "x_:\n" << x_ << std::endl;
-std::cout << "P_:\n" << P_ << std::endl;
-//std::cin >> is_initialized_;
+#if (TEST==1)
+	std::cout << "x_:\n" << x_ << std::endl;
+	std::cout << "P_:\n" << P_ << std::endl;
+	//std::cin >> is_initialized_;
+#endif
 }
 
 /**
@@ -158,9 +162,21 @@ void UKF::Prediction(double delta_t) {
 
   //std::cout << "This delta_t: " << delta_t << std::endl;
   GenerateSigmaPoints();
+#if (TEST==1)
+//	std::cout << "Xsig_aug:\n" << Xsig_aug << std::endl;
+#endif
+
   PredictSigmaPoints(delta_t);
-  PredictMeanAndCovariance();  //MANUALLY unit tested to here
-   
+#if (TEST==1)
+//	std::cout << "Xsig_pred_:\n" << Xsig_pred_ << std::endl;
+#endif
+
+  PredictMeanAndCovariance();
+#if (TEST==1)
+//  std::cout << "x_(k+1|k):\n" << x_ << std::endl;
+//  std::cout << "P_(k+1|k):\n" << P_ << std::endl;
+//  std::cin >> lambda_;
+#endif   
 }
 
 void UKF::GenerateSigmaPoints() {
@@ -170,22 +186,6 @@ void UKF::GenerateSigmaPoints() {
   // Create the augmented state vector be adding the means of the process noise and 
   // create the augmented covariance matrix to include the process noise covariance
   // *************************************************************************************
-
-#if (TEST==1)
-  //set example state
-  x_ <<   5.7441,
-         1.3800,
-         2.2049,
-         0.5015,
-         0.3528;
-
-  //create example covariance matrix
-  P_ <<     0.0043,   -0.0013,    0.0030,   -0.0022,   -0.0020,
-          -0.0013,    0.0077,    0.0011,    0.0071,    0.0060,
-           0.0030,    0.0011,    0.0054,    0.0007,    0.0008,
-          -0.0022,    0.0071,    0.0007,    0.0098,    0.0100,
-          -0.0020,    0.0060,    0.0008,    0.0100,    0.0123;
-#endif
 
   //create augmented mean vector
   VectorXd x_aug_ = VectorXd(n_aug_);
@@ -230,19 +230,11 @@ void UKF::GenerateSigmaPoints() {
 
 #if (TEST==1)  
   //std::cout << delta_t << std::endl;
-  //std::cout << Xsig_aug << std::endl;
-  //std::cout << P_aug << std::endl;
+  //std::cout << "Xsig_aug:\n" << Xsig_aug << std::endl;
+  //std::cout << "P_aug:\n" << P_aug << std::endl;
   //std::cin >> lambda_;
 #endif  
-//  expected result:
-//Xsig_aug =
-//5.7441 5.85768 5.7441 5.7441 5.7441 5.7441 5.7441 5.7441 5.63052 5.7441 5.7441 5.7441 5.7441 5.7441 5.7441
-//1.38 1.34566 1.52806 1.38 1.38 1.38 1.38 1.38 1.41434 1.23194 1.38 1.38 1.38 1.38 1.38
-//2.2049 2.28414 2.24557 2.29582 2.2049 2.2049 2.2049 2.2049 2.12566 2.16423 2.11398 2.2049 2.2049 2.2049 2.2049
-//0.5015 0.44339 0.631886 0.516923 0.595227 0.5015 0.5015 0.5015 0.55961 0.371114 0.486077 0.407773 0.5015 0.5015 0.5015
-//0.3528 0.299973 0.462123 0.376339 0.48417 0.418721 0.3528 0.3528 0.405627 0.243477 0.329261 0.22143 0.286879 0.3528 0.3528
-//0 0 0 0 0 0 0.34641 0 0 0 0 0 0 -0.34641 0 
-//0 0 0 0 0 0 0 0.34641 0 0 0 0 0 0 -0.34641
+
 
 }
 
@@ -252,18 +244,7 @@ void UKF::PredictSigmaPoints(double delta_t) {
   // *************************************************************************************
   // Plug in the sigma points to the process model to calculate Xsig_pred
   // *************************************************************************************  
-#if (TEST==1)
-     Xsig_aug <<
-    5.7441,  5.85768,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.63052,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,
-      1.38,  1.34566,  1.52806,     1.38,     1.38,     1.38,     1.38,     1.38,   1.41434,  1.23194,     1.38,     1.38,     1.38,     1.38,     1.38,
-    2.2049,  2.28414,  2.24557,  2.29582,   2.2049,   2.2049,   2.2049,   2.2049,   2.12566,  2.16423,  2.11398,   2.2049,   2.2049,   2.2049,   2.2049,
-    0.5015,  0.44339, 0.631886, 0.516923, 0.595227,   0.5015,   0.5015,   0.5015,   0.55961, 0.371114, 0.486077, 0.407773,   0.5015,   0.5015,   0.5015,
-    0.3528, 0.299973, 0.462123, 0.376339,  0.48417, 0.418721,   0.3528,   0.3528,  0.405627, 0.243477, 0.329261,  0.22143, 0.286879,   0.3528,   0.3528,
-         0,        0,        0,        0,        0,        0,  0.34641,        0,         0,        0,        0,        0,        0, -0.34641,        0,
-         0,        0,        0,        0,        0,        0,        0,  0.34641,         0,        0,        0,        0,        0,        0, -0.34641;
-		 
-    delta_t = 0.1; //seconds
-#endif
+
   double px_, py_, v_, psi_, psid_, nu_a_, nu_psidd_;
   
   //std::cout << "delta_t: " << delta_t;
@@ -290,27 +271,22 @@ void UKF::PredictSigmaPoints(double delta_t) {
     if (fabs(psid_)< 0.001) { //avoid division by zero
       Xsig_pred_(0,sigpt) = px_   + v_*cos(psi_)*delta_t + 0.5*delta_t*delta_t*cos(psi_)*nu_a_;
       Xsig_pred_(1,sigpt) = py_   + v_*sin(psi_)*delta_t + 0.5*delta_t*delta_t*sin(psi_)*nu_a_;
-      Xsig_pred_(2,sigpt) = v_    + 0                    + delta_t*nu_a_;
+      Xsig_pred_(2,sigpt) = v_    + 0.                   + delta_t*nu_a_;
       Xsig_pred_(3,sigpt) = psi_  + psid_*delta_t        + 0.5*delta_t*delta_t*nu_psidd_;
-      Xsig_pred_(4,sigpt) = psid_ + 0                    + delta_t*nu_psidd_;
+      Xsig_pred_(4,sigpt) = psid_ + 0.                   + delta_t*nu_psidd_;
     } else {
       Xsig_pred_(0,sigpt) = px_   + v_/psid_*(sin(psi_+psid_*delta_t)-sin(psi_))  + 0.5*delta_t*delta_t*cos(psi_)*nu_a_;
       Xsig_pred_(1,sigpt) = py_   + v_/psid_*(-cos(psi_+psid_*delta_t)+cos(psi_)) + 0.5*delta_t*delta_t*sin(psi_)*nu_a_;
-      Xsig_pred_(2,sigpt) = v_    + 0                                             + delta_t*nu_a_;
+      Xsig_pred_(2,sigpt) = v_    + 0.                                            + delta_t*nu_a_;
       Xsig_pred_(3,sigpt) = psi_  + psid_*delta_t                                 + 0.5*delta_t*delta_t*nu_psidd_;
-      Xsig_pred_(4,sigpt) = psid_ + 0                                             + delta_t*nu_psidd_;
+      Xsig_pred_(4,sigpt) = psid_ + 0.                                            + delta_t*nu_psidd_;
     }
   }
 #if (TEST==1)  
   //std::cout << "Xsig_pred_: \n" << Xsig_pred_ << std::endl;
   //std::cin >> px_;
 #endif
-//Xsig_pred =
-//5.93553 6.06251 5.92217 5.9415 5.92361 5.93516 5.93705 5.93553 5.80832 5.94481 5.92935 5.94553 5.93589 5.93401 5.93553
-//1.48939 1.44673 1.66484 1.49719 1.508 1.49001 1.49022 1.48939 1.5308 1.31287 1.48182 1.46967 1.48876 1.48855 1.48939
-//2.2049 2.28414 2.24557 2.29582 2.2049 2.2049 2.23954 2.2049 2.12566 2.16423 2.11398 2.2049 2.2049 2.17026 2.2049
-//0.53678 0.473387 0.678098 0.554557 0.643644 0.543372 0.53678 0.538512 0.600173 0.395462 0.519003 0.429916 0.530188 0.53678 0.535048
-//0.3528 0.299973 0.462123 0.376339 0.48417 0.418721 0.3528 0.387441 0.405627 0.243477 0.329261 0.22143 0.286879 0.3528 0.318159  
+
 }
 
 void UKF::PredictMeanAndCovariance() {
@@ -319,24 +295,25 @@ void UKF::PredictMeanAndCovariance() {
   // *************************************************************************************
   // Calculate the mean and covariance of Xsig_pred
   // *************************************************************************************  
-#if (TEST==1)
-        Xsig_pred_ <<
-         5.9374,  6.0640,   5.925,  5.9436,  5.9266,  5.9374,  5.9389,  5.9374,  5.8106,  5.9457,  5.9310,  5.9465,  5.9374,  5.9359,  5.93744,
-           1.48,  1.4436,   1.660,  1.4934,  1.5036,    1.48,  1.4868,    1.48,  1.5271,  1.3104,  1.4787,  1.4674,    1.48,  1.4851,    1.486,
-          2.204,  2.2841,  2.2455,  2.2958,   2.204,   2.204,  2.2395,   2.204,  2.1256,  2.1642,  2.1139,   2.204,   2.204,  2.1702,   2.2049,
-         0.5367, 0.47338, 0.67809, 0.55455, 0.64364, 0.54337,  0.5367, 0.53851, 0.60017, 0.39546, 0.51900, 0.42991, 0.530188,  0.5367, 0.535048,
-          0.352, 0.29997, 0.46212, 0.37633,  0.4841, 0.41872,   0.352, 0.38744, 0.40562, 0.24347, 0.32926,  0.2214, 0.28687,   0.352, 0.318159;
-		x_.fill(0.0);
-		P_.fill(0.0);
-#endif    
   
-  //predict state mean
+  //predict new state mean
+	x_.fill(0.);
     for (int i=0;i<n_sig_; i++) {
-        x_ += weights_(i) * Xsig_pred_.col(i);
+#if (TEST==1)
+//	std::cout << "i: " << i << std::endl;
+//	std::cout << "x_:\n" << x_ << std::endl;
+//	std::cout << "weights_(i): " << weights_(i) << std::endl;
+//	std::cout << "Xsig_pred_.col(i):\n" << Xsig_pred_.col(i) << std::endl;
+//	std::cout << "Expected x_:\n" << x_ + (weights_(i) * Xsig_pred_.col(i)) << std::endl;
+#endif
+        x_ = x_ + (weights_(i) * Xsig_pred_.col(i));
+#if (TEST==1)
+//	std::cout << "x_:\n" << x_ << std::endl;
+#endif
     }
 
   VectorXd temp2 = VectorXd(n_x_);
-
+    P_.fill(0.);
     for (int i=0; i<n_sig_; i++) {
         temp2 = Xsig_pred_.col(i) - x_;
         P_ += weights_(i) * temp2 * temp2.transpose();
